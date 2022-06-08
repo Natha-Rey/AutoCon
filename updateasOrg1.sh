5f5c18fb88e05d266d168ca2a5e5c286eff6a31c
Gazi Md Ashab Hossain <takveer@gmail.com>
2022-06-07 20:35:19 -0400

    Add files via upload

---
+++ updateasOrg1.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+# Go to the network folder
+pushd ~/AutoCon/network
+
+
+# set cfg path
+export FABRIC_CFG_PATH=$PWD/../config
+
+
+
+# Set environment to Org1 - SFX
+export CORE_PEER_TLS_ENABLED=true
+export CORE_PEER_LOCALMSPID="Org1MSP"
+export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/SFX.autocon.net/peers/peer0.SFX.autocon.net/tls/ca.crt
+export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/SFX.autocon.net/users/Admin@SFX.autocon.net/msp
+export CORE_PEER_ADDRESS=localhost:7051
+
+
+# run 'updateJob' command
+
+peer chaincode invoke -o localhost:7050 --ordererTLSHostnameOverride orderer.autocon.net --tls --cafile ${PWD}/organizations/ordererOrganizations/autocon.net/orderers/orderer.autocon.net/msp/tlscacerts/tlsca.autocon.net-cert.pem -C channel001 -n jobcontract --peerAddresses localhost:7051 --tlsRootCertFiles ${PWD}/organizations/peerOrganizations/SFX.autocon.net/peers/peer0.SFX.autocon.net/tls/ca.crt --peerAddresses localhost:9051 --tlsRootCertFiles ${PWD}/organizations/peerOrganizations/JPL.autocon.net/peers/peer0.JPL.autocon.net/tls/ca.crt -c '{"Args":["updateJob", "SFX", "002", "Clean Kitchen sink and replace pipes", "Road 3, Richmond Hill", "67.25", "20"]}'
